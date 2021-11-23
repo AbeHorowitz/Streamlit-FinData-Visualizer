18c9a6cc822d06c3927a9843073b78b0943a050e
AbeHorowitz <93952060+AbeHorowitz@users.noreply.github.com>
2021-11-23 00:40:04 -0500

    Add files via upload
    Update and final. Fake variables inserted for any proprietary information

--- get_Data.py
+++ get_Data.py
@@ -4,48 +4,62 @@ import numpy as np
 
 from datetime import date
 from datetime import timedelta
-import dateutil.easter
+from datetime import datetime
+from dateutil.easter import easter
 import holidays
 
 c = p.Client(api_token='Tpk_057363474545412b9a53956f425fa12d', version='sandbox') #using Sandbox API token so not counting against monthly limit
 
-def get_data(symbol, start_date, end_date):
-	
-	df = c.chartDF(symbol='TSLA', timeframe='1y')[['close', 'volume']].loc[start_date:end_date]
-	year_1 = start_date[:4] #these variables will be used again later
-	year_2 = end_date[:4]
-	#Streamlit.date_input returns a date in ISO 8601 format of year-month-day such as 2021-11-02
-	s = [year_1, start_date[5:7], start_date[8:]]
-	e = [year_2, end_date[5:7], end_date[8:]]
+def get_data(symbol, start_date, end_date): # Inclusive on both upper/lower bounds
+	# 1) Get Stock info
+	# 1a) An unfortunately necessary pre-req: While Secret API is inclusive on both bounds, pyEX is exclusive on lower bound.
+	# Begin by correcting this in all cases, as need be. '2021-05-25'
+	temp = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=-1)
+	df_start_date = temp.strftime('%Y-%m-%d')
 
-	start = ""
-	end = ""
-	
-	for i in s:
-		start += i+":"
-	for i in e:
-		end += i+":"
+	df = c.chartDF(symbol='TSLA', timeframe='1y')[['close', 'volume']].loc[df_start_date:end_date]
 
+	#2) Convert OG date to date good for secret_stat API/Streamlit.date_input returns a date in ISO 8601 format of year-month-day such as 2021-11-02
+	year1, year2 = start_date[:4], end_date[:4] # These variables will be used again later
+	s, e = [year1, start_date[5:7], start_date[8:]], [year2, end_date[5:7], end_date[8:]]
+	start, end = "", ""
+	for i in s: start += i+":"
+	for i in e: end += i+":"
+
+	#3) get data
 	params = {'ticker': symbol, 'from_datetime': start+'08:30:00', 'until_datetime' : end+'08:30:00', 'agg_mode':'D'}
+	secret_stat_init = requests.get('Secret API Link', params=params).json()
+	
+	#4) match up secret_stat data indices with DataFrame date indices
+	secret_stat, secret_stat_time = np.array([]), []
+	holidays1, holidays2 = createHolidayCalendar(year1), None
+	if year1!=year2: holidays2 = createHolidayCalendar(year2)# If necessary, also set up year2 holidays for the second year
 
-	stat_init = requests.get('secret API link', params=params).json() #in the real code, there is a proprietary, non-publishable statistic here
-	stat = np.array([])
-	stat_time = []
-
-	holidays = createHolidayCalendar('year1')
-
-	for i in pulse_init:
+	#run isValidTradingDay depending on which year. More efficient: set up
+	#isValidTrading Day to take in isodate, a dictionary with holiday calendars. Depending on year in isodate,
+	#use one of the holiday calendars.
+	#OR, just redo this piece to use the Pandas library instead: https://stackoverflow.com/questions/2394235/detecting-a-us-holiday
+	for i in secret_stat_init:
 		for j in i.keys():
-			if isValidTradingDay(j[:10], holidays):
-				stat_time.append(j)
-				pulse = np.append(stat, i[j])
+			if j[:4]==year1:
+				if isValidTradingDay(j[:10], holidays1):
+					secret_stat_time.append(j)
+					secret_stat = np.append(secret_stat, i[j])
+			elif j[:4]==year2:
+				if isValidTradingDay(j[:10], holidays2):
+					secret_stat_time.append(j)
+					secret_stat = np.append(secret_stat, i[j])
 
-	df['Secret Proprietary Statistic'] = stat
-
-	df.reset_index(inplace=True)
-
-
-	print(df)
+	# 5) Finish: If the datasets matchup, write the dataframe to a file to be used elsewhere, to avoid overcalling Athena API 
+	# If the datasets don't matchup, then inform developer to correct error.
+	try: 
+		df['secret_stat'] = secret_stat
+		df.reset_index(inplace=True)
+		df.to_pickle("TeslaDFActual.pkl")
+	except ValueError:
+		for i in secret_stat_time:
+			if i[:10] not in [i.strftime('%Y-%m-%d') for i in df.index]:
+				print("The following date was included in the secret_stat dataset but not included in the equity data. Please review isValidTradingDay method:", i)
 
 def isValidTradingDay(isodate, holidays):
 	day = date.fromisoformat(isodate).isoweekday()
@@ -58,8 +72,7 @@ def isValidTradingDay(isodate, holidays):
 
 # Select country
 def createHolidayCalendar(year):
-	year = float(year)
-	us_holidays = holidays.US(years=year)
+	us_holidays = holidays.US(years=int(year))
 	us_holidays.pop_named('Veterans Day')
 	us_holidays.pop_named('Columbus Day')
 	try:
@@ -71,11 +84,11 @@ def createHolidayCalendar(year):
 	except KeyError:
 		pass
 
-	good_friday = dateutil.easter.easter(year) + timedelta(days=-2)
+	good_friday = easter(int(year)) + timedelta(days=-2)
 
 	us_holidays.append({good_friday:'Good Friday'})
 
 	return(us_holidays)
 
-get_data('TSLA', '2021-10-10', '2021-10-21')
+get_data('TSLA', '2021-05-10', '2021-10-15')
 
