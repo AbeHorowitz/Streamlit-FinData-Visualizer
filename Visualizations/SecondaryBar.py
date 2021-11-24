0e08a93ba160c2bac3a2acea43471b193c7f40b0
AbeHorowitz <93952060+AbeHorowitz@users.noreply.github.com>
2021-11-23 23:38:40 -0500

    Add files via upload

---
+++ Visualizations/SecondaryBar.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+df = pd.read_pickle("TeslaDFActual.pkl")
+
+time = df.iloc[:,0].values
+close = df.iloc[:,1].values
+volume = df.iloc[:,2].values
+
+# Create figure with secondary y-axis
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+# Add traces
+fig.add_trace(
+    go.Scatter(x=time, y=close, name="Close Price"),
+    secondary_y=False,
+)
+
+fig.add_trace(
+    go.Bar(x=time, y=volume, name="Volume"),
+    secondary_y=True,
+)
+
+# Add figure title
+fig.update_layout(
+    title_text="Close Price vs. Volume"
+)
+
+# Set x-axis title
+fig.update_xaxes(title_text="Time")
+
+# Set y-axes titles
+fig.update_yaxes(title_text="Close Price Data", secondary_y=False)
+fig.update_yaxes(title_text="Volume Data", secondary_y=True)
+
+fig.show()
